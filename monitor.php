c80f82626177a642494af41825f2968c69f7ee97
Jefta Dirksen <jeftadirksen@gmail.com>
2019-08-03 11:26:32 +0200

    Mailer class

--- monitor.php
+++ monitor.php
@@ -3,12 +3,6 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     
-    require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/src/SMTP.php';
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-
     spl_autoload_register(function ($class_name) {
         include $class_name . '.class.php';
     });
@@ -30,40 +24,17 @@
             }
             echo $msg;
             if(!$monitor->getFailed()) {
-                sendMail($msg);
+                $mail = new Mailer();
+                $mail->send("PnPMonitor failure", $msg);
                 $monitor->setFailed(true);
             }
         }
         else {
             printf("Monitor %d OK\n",$monitor->id);
             if($monitor->getFailed()) {
-                sendMail("Restored");
+                $mail = new Mailer();
+                $mail->send("PnPMonitor restored", "Restored");
                 $monitor->setFailed(false);
             }
-        }
-    }
-
-    function sendMail($message) {
-        global $config;
-        try {
-            $mail = new PHPMailer(true);
-            //$mail->SMTPDebug = 2;
-            $mail->isSMTP();
-            $mail->SMTPAuth     = true;
-            $mail->Host         = $config['SMTP_HOST'];
-            $mail->Username     = $config['SMTP_USER'];
-            $mail->Password     = $config['SMTP_PASS'];
-            $mail->SMTPSecure   = 'tls';
-            $mail->Port         = 587;
-            
-            $mail->setFrom($config['SMTP_FROM']);
-            $mail->addAddress($config['SMTP_TO']);
-            $mail->Subject = "PnPMonitor failure";
-            $mail->Body = $message;
-            $mail->send();
-            echo "Mail sent.\n";
-        }
-        catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}\n";
         }
     }
