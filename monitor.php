6cd5b34072ad4eca4dc00e1452fcc81b0127fe43
Jefta Dirksen <jeftadirksen@gmail.com>
2019-08-04 11:02:27 +0200

    monitor.php not available from web

--- monitor.php
+++ monitor.php
@@ -1,4 +1,6 @@
 <?php
+    if (php_sapi_name() != "cli") die("monitor.php should be run from command line.");
+
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
