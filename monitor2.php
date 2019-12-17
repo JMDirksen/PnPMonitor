194858eeffe0b408564e24688beebe3364423995
Jefta Dirksen <jeftadirksen@gmail.com>
2019-12-17 22:52:38 +0100

    successCount, failCount, db setup

--- monitor2.php
+++ monitor2.php
@@ -10,16 +10,60 @@
 
     $db = loadDb($config['DB_FILE']);
 
+    // Check db
+    if(!isset($db->sendMailAtXFails)) $db->sendMailAtXFails = 3;
+    if(!isset($db->sendMailAtXSuccesses)) $db->sendMailAtXSuccesses = 2;
+    if(!isset($db->monitors)) $db->monitors = [];
+
     // Reset monitors / test data
-    $db->monitors = [];
-    addMonitor($db, portMonitor("GooglePort", "google.nl", 80));
-    addMonitor($db, pageMonitor("GooglePage", "http://www.google.nl", "zoeken"));
+    //$db->monitors = [];
+    //addMonitor($db, portMonitor("GooglePort", "google.nl", 80));
+    //addMonitor($db, pageMonitor("GooglePage", "http://www.google.nl", "zoeken"));
 
     foreach($db->monitors as $key => $monitor) {
+        // Check monitor
+        if(!isset($monitor->failing)) $monitor->failing = false;
+        if(!isset($monitor->successCount)) $monitor->successCount = 0;
+        if(!isset($monitor->failCount)) $monitor->failCount = 0;
+        
         echo "Monitor: ".$monitor->name.PHP_EOL;
         $result = testMonitor($monitor);
         echo "Result: ".$result.PHP_EOL;
-        $db->monitors[$key]->lastResult = $result;
+        $monitor->lastResult = $result;
+        
+        if($result === false) {
+            $monitor->successCount = 0;
+            $monitor->failCount++;
+            switch($monitor->type) {
+                case "page":
+                    $string = "Page %s failed to load correctly!\n";
+                    $msg = sprintf($string, $monitor->url);
+                    break;
+                case "port":
+                    $string = "Port %s:%d isn't accepting connections!\n";
+                    $msg = sprintf($string, $monitor->host, $monitor->port);
+                    break;
+            }
+            if($monitor->failCount >= $db->sendMailAtXFails && !$monitor->failing) {
+                $monitor->failing = true;
+                //$mail = new Mailer();
+                //$mail->send("PnPMonitor failed - $monitor->name", $msg);
+                echo "Mail sent.\n";
+            }
+        }
+        else {
+            $monitor->successCount++;
+            if($monitor->successCount >= $db->sendMailAtXSuccesses && $monitor->failing) {
+                $monitor->failing = false;
+                $monitor->failCount = 0;
+                //$mail = new Mailer();
+                //$mail->send("PnPMonitor restored - $monitor->name",
+                //    "Monitor $monitor->name has been restored\n");
+                echo "Mail sent.\n";
+            }
+        }
+        
+        $db->monitors[$key] = $monitor;
     }
 
     saveDb($db, $config['DB_FILE']);
@@ -27,7 +71,9 @@
 
 
     function loadDb($dbFile) {
-        return json_decode(file_get_contents($dbFile));
+        $db = json_decode(@file_get_contents($dbFile));
+        if(!$db) $db = (object) null;
+        return $db;
     }
 
     function saveDb(&$db, $dbFile) {
