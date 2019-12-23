aef29636d6003779c42155a32ced356c04764048
Jefta Dirksen <jeftadirksen@gmail.com>
2019-12-23 18:04:41 +0100

    Style

--- index.php
+++ index.php
@@ -68,30 +68,29 @@ elseif(isset($_SESSION['id'])) {
     echo '<table><tr><th>Name</th><th>URL/Host</th><th>Text/Port</th><th> </th></tr>';
     echo '<tr><form action="action.php" method="post">';
     echo '<input type="hidden" name="type" value="page">';
-    echo '<td><input type="text" name="name" placeholder="name" required';
+    echo '<td><input type="text" class="name" name="name" placeholder="name" required';
     if(isset($_SESSION['pagename'])) echo ' value="'.$_SESSION['pagename'].'"';
     echo '></td>';
-    echo '<td><input type="text" name="url" placeholder="http(s)://" required';
+    echo '<td><input type="text" class="urlhost" name="url"';
     if(isset($_SESSION['url'])) echo ' value="'.$_SESSION['url'].'"';
-    echo '></td>';
-    echo '<td><input type="text" name="text" placeholder="should contain this '.
-         'text"';
+    echo ' placeholder="http(s)://" required></td>';
+    echo '<td><input type="text" class="textport" name="text"';
     if(isset($_SESSION['text'])) echo ' value="'.$_SESSION['text'].'"';
-    echo '></td>';
+    echo ' placeholder="should contain text"></td>';
     echo '<td><input type="submit" name="addPage" value="Add Page Monitor"></td>';
     echo '</form></tr>';
 
     // Add Port monitor form
     echo '<tr><form action="action.php" method="post">';
     echo '<input type="hidden" name="type" value="port">';
-    echo '<td><input type="text" name="name" placeholder="name" required';
+    echo '<td><input type="text" class="name" name="name" placeholder="name"';
     if(isset($_SESSION['portname'])) echo ' value="'.$_SESSION['portname'].'"';
-    echo '></td>';
-    echo '<td><input type="text" name="host" placeholder="host" required';
+    echo ' required></td>';
+    echo '<td><input type="text" class="urlhost" name="host"';
     if(isset($_SESSION['host'])) echo ' value="'.$_SESSION['host'].'"';
-    echo '></td>';
-    echo '<td><input type="number" min=1 max=65535 name="port" placeholder="port" '.
-         'required';
+    echo ' placeholder="host" required></td>';
+    echo '<td><input type="number" class="textport" min=1 max=65535 '.
+         'name="port" placeholder="port" required';
     if(isset($_SESSION['port'])) echo ' value="'.$_SESSION['port'].'"';
     echo '></td>';
     echo '<td><input type="submit" name="addPort" value="Add Port Monitor"></td>';
