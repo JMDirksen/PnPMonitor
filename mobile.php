7907ded627654a9fca905494b33b2c87bbd78538
Jefta Dirksen <jeftadirksen@gmail.com>
2020-01-04 22:06:18 +0100

    start mobile interface

---
+++ mobile.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Mobile</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="mobile.css">
+</head>
+<body>
+<div class="header">Page and Port Monitor</div>
+<ul>
+<li><a href="">Monitors</a></li>
+<li><a href="">Settings</a></li>
+<li><a href="">Logout</a></li>
+</ul>
+</body>
+</html>
