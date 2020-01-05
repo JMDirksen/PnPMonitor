361e8c42e1f1dd9d2e2232c0d1967813a610a41f
Jefta Dirksen <jeftadirksen@gmail.com>
2020-01-05 12:37:12 +0100

    Web  app manifest

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <title>Menu</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="style.css">
+<link rel="manifest" href="manifest.json">
 </head>
 <body>
 <div id="header">Page and Port Monitor</div>
