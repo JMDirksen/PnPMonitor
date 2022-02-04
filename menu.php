0e0662a53bc9b1ae54eedcd6dcd42519a6698dff
Jefta Dirksen <jeftadirksen@gmail.com>
2022-02-04 16:20:24 +0100

    confirm logout

--- menu.php
+++ menu.php
@@ -1,11 +1,15 @@
 <?php loginRequired(); ?>
 <div id="button-bar">
-    <div class="button"></div>
-    <div class="button"><a href="action.php?logout">Logout</a></div>
+  <div class="button"></div>
+  <div class="button">
+    <a href="action.php?logout" onClick="return confirm('Logout?');">
+      Logout
+    </a>
+  </div>
 </div>
 <?php showMessage(); ?>
 <div id="menu">
-<div class="menu-item"><a href="?p=monitors">Monitors</a></div>
-<div class="menu-item"><a href="?p=settings">Settings</a></div>
-<div class="menu-item"><a href="?p=users">Users</a></div>
+  <div class="menu-item"><a href="?p=monitors">Monitors</a></div>
+  <div class="menu-item"><a href="?p=settings">Settings</a></div>
+  <div class="menu-item"><a href="?p=users">Users</a></div>
 </div>
