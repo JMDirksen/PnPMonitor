840aa7566ab1b7b22006ade423f78dfbf8388279
Jefta Dirksen <jeftadirksen@gmail.com>
2022-02-04 15:06:11 +0100

    100 day session lifetime

--- init.php
+++ init.php
@@ -4,6 +4,7 @@ ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
 
+ini_set('session.cookie_lifetime', 60 * 60 * 24 * 100);
 session_start();
 
 require_once("functions.php");
