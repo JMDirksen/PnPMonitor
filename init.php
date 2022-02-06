0fbbc179c4333cf4c36c10c26114d463d83f8a6d
Jefta Dirksen <jeftadirksen@gmail.com>
2022-02-06 10:53:47 +0100

    Refresh session lifetime

--- init.php
+++ init.php
@@ -7,8 +7,10 @@ error_reporting(E_ALL);
 $ssp = session_save_path().'/pnpmonitor';
 if(!is_dir($ssp)) mkdir($ssp);
 ini_set('session.save_path', $ssp);
-ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 100);
-ini_set('session.cookie_lifetime', 60 * 60 * 24 * 100);
+$lifetime = 60*60*24*90;
+ini_set('session.gc_maxlifetime', $lifetime);
+ini_set('session.cookie_lifetime', $lifetime);
 session_start();
+setcookie(session_name(), session_id(), time()+$lifetime);
 
 require_once("functions.php");
