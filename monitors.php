8e3c50a832bb3cbb09d15fe9ec97b2ca1d2e06a9
JMDirksen <jeftadirksen@gmail.com>
2022-09-22 15:11:03 +0200

    Sort monitors by name

--- monitors.php
+++ monitors.php
@@ -1,19 +1,22 @@
 <div id="button-bar">
-  <div class="button"><a href="?p=menu">Back</a></div>
-  <div class="button"><a href="?p=edit&new"><b>+</b></a></div>
+    <div class="button"><a href="?p=menu">Back</a></div>
+    <div class="button"><a href="?p=edit&new"><b>+</b></a></div>
 </div>
 <div id="menu">
-  <?php
-  loginRequired();
-  $db = loadDb(false);
-  foreach ($db->monitors as $monitor) {
-    $id = $monitor->id;
-    $name = $monitor->name;
-    $response = $monitor->lastResult;
-    $fail = $response == -1 ? "fail" : "";
-  ?>
-    <div class="menu-item">
-      <a href="?p=monitor&id=<?php echo $id; ?>"><?php echo $name; ?><span class="status <?php echo $fail; ?>"><?php echo $response; ?></span></a>
-    </div>
-  <?php } ?>
+    <?php
+    loginRequired();
+    $db = loadDb(false);
+    usort($db->monitors, function ($a, $b) {
+        return strcmp($a->name, $b->name);
+    });
+    foreach ($db->monitors as $monitor) {
+        $id = $monitor->id;
+        $name = $monitor->name;
+        $response = $monitor->lastResult;
+        $fail = $response == -1 ? "fail" : "";
+    ?>
+        <div class="menu-item">
+            <a href="?p=monitor&id=<?php echo $id; ?>"><?php echo $name; ?><span class="status <?php echo $fail; ?>"><?php echo $response; ?></span></a>
+        </div>
+    <?php } ?>
 </div>
